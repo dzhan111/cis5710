e143732d6e4da5e8b5c5cb63ed1dbc8167a6b839
dzhan111 <zhandavid4@gmail.com>
2026-01-27 06:00:17 +0000

    hw 2a

--- hw2a-divider/DividerUnsigned.sv
+++ hw2a-divider/DividerUnsigned.sv
@@ -1,4 +1,4 @@
-/* INSERT NAME AND PENNKEY HERE */
+/* David Zhan 63555686 */
 
 `timescale 1ns / 1ns
 
@@ -10,8 +10,32 @@ module DividerUnsigned (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
+    wire [31:0] dividend_pipe  [0:32];
+    wire [31:0] remainder_pipe [0:32];
+    wire [31:0] quotient_pipe  [0:32];
 
-    // TODO: your code here
+    assign dividend_pipe[0]  = i_dividend;
+    assign remainder_pipe[0] = 32'b0;
+    assign quotient_pipe[0]  = 32'b0;
+
+    genvar k;
+    generate
+        for (k = 0; k < 32; k = k + 1) begin : gen_div
+            DividerOneIter u_iter (
+                .i_dividend  (dividend_pipe[k]),
+                .i_divisor   (i_divisor),
+                .i_remainder (remainder_pipe[k]),
+                .i_quotient  (quotient_pipe[k]),
+                .o_dividend  (dividend_pipe[k+1]),
+                .o_remainder (remainder_pipe[k+1]),
+                .o_quotient  (quotient_pipe[k+1])
+            );
+        end
+    endgenerate
+
+    assign o_remainder = remainder_pipe[32];
+    assign o_quotient  = quotient_pipe[32];
+
 
 endmodule
 
@@ -39,5 +63,13 @@ module DividerOneIter (
     */
 
     // TODO: your code here
+    wire [31:0] rem_shift = (i_remainder << 1) | {31'b0, i_dividend[31]};
+
+    wire take = ($unsigned(rem_shift) >= $unsigned(i_divisor));
+    
+    assign o_quotient = (i_quotient << 1) | {31'b0, take};
+    assign o_remainder = take ? (rem_shift - i_divisor) : rem_shift;
+    assign o_dividend = (i_dividend << 1);
+
 
 endmodule
